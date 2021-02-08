23afa752fcc49a99b0766dab261eae5bba081527
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-08 00:11:34 -0800

    Added endpoint for getting period of given time

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ This endpoint returns the entire schedule for all five days of the week. \
 Parameters:
 * `block` (optional): Get timings for a time block in each day's schedule. Example: `p1` for period one.
 
-### `/day`
+### `/day/<day>`
 This endpoint returns the schedule for a day. \
 Parameters: 
 * `block` (optional): Get timings for a time block in the day's schedule. Example: `p1` for period one.
+
+### `/day/<day>/time/<time>`
+This endpoint returns the period for a given time and day. For example, if you wanted to know what period is at 9:30 on Monday, you could make a request to `/day/monday/time/09:30`.
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import flask
+from datetimerange import DateTimeRange
+from datetime import date
 from json import loads
 
 app = flask.Flask(__name__)
+
+def get_schedule(day=None):
+    schedule = loads(
+        open(
+            'schedule.json',
+            'r'
+        ).read()
+    )
+    if day:
+        return(schedule[day])
+    else:
+        return(schedule)
+
+def check_range(block, day, to_verify):
+    today = str(date.today())
+    block_timings = get_schedule(day)[block]
+    start = block_timings['start']
+    end = block_timings['end']
+    time_range = DateTimeRange(f"{today}T{start}:00+0800", f"{today}T{end}:00+0800")
+    print(f"{today}T{to_verify}:00+0800" in time_range)
+    return(f"{today}T{to_verify}:00+0800" in time_range)
 
 @app.route('/all')
 def all():
@@ -57,5 +80,21 @@ def day(day):
         }
 
     return(res)
-    
-app.run()
+
+@app.route('/day/<day>/time/<time>')
+def time(day, time):
+    schedule = get_schedule(day=day)
+    del schedule['passing_periods']
+    for block in schedule:
+        if check_range(block, day, time):
+            return({
+                'data': {
+                    block: schedule[block]
+                }
+            })
+    return({
+        'data': None
+    })
+            
+
+app.run(debug=True)
